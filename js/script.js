d836a07826fa5fbae50182e4a359cd04d88270e6
Luca Giozza <luca.giozza@gmail.com>
2021-06-14 15:34:59 +0200

    completamento esercizio

--- js/script.js
+++ js/script.js
@@ -9,19 +9,65 @@
 // di aggiungere un nuovo oggetto studente inserendo nell’ordine:
 //  nome, cognome e età.
 
-// creo un oggetto che decriva lo studente
+// creo un oggetto che descriva lo studente
 var studente = {
     "nome" : 'Luca',
     "cognome" : 'Giozza',
     "età" : 22
 };
-console.log(studente)
+
 
 
 for ( var k in studente){
-   console.log(studente[k])
+   console.log(studente[k]);
 }
 
 
 
-var alunni= []
+var alunni= [
+
+  {
+   'nome' : 'Matteo',
+   'cognome' : 'Bianchi',
+   'età' : 18
+  },
+  {
+    'nome' : 'Simone' ,
+    'cognome' : 'Cristoforetti',
+    'età' : 25
+  },
+  {
+    'nome' : 'Alessia',
+    'cognome' : 'Mocellini',
+    'età' : 21
+  },
+  {
+   'nome' :  'x' ,
+   'cognome' : 'x', 
+   'età' :  'x'
+
+  }
+
+];
+
+
+document.getElementById('primo').innerHTML =' Primo studente : ' + ' ' +  alunni[0].nome + ' ' +  alunni[0].cognome ;
+
+
+document.getElementById('secondo').innerHTML =' Secondo studente :  ' + ' ' +  alunni[1].nome + ' ' + alunni[1].cognome ;
+
+
+document.getElementById('terzo').innerHTML =' Terzo studente : ' + ' ' +  alunni[2].nome + ' ' +  alunni[2].cognome;
+
+
+
+var inserisciNome = prompt('inserisci nome studente');
+var inserisciCognome= prompt('inserisci cognome studente');
+var inserisciEta = parseInt(prompt('inserisci età studente'));
+
+
+alunni[3].nome = inserisciNome;
+alunni[3].cognome = inserisciCognome;
+alunni[3].età = inserisciEta;
+
+document.getElementById('scuola').innerHTML = 'Quarto studente' + ' ' + alunni[3].nome + ' ' + alunni[3].cognome + ' ' + alunni[3].età + ' anni'  ;
